7fa04bcf960904b3f539a2a43ca631fdaaa6bb25
Harpreet Kaur <harpreetkaurguglani@gmail.com>
2022-06-10 12:55:14 -0700

    value function added

---
+++ src/code/ValueTables_function.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import math
+
+def ValueTable(indicator):
+    if indicator == "GDP":
+        IndicatorValues = pd.read_csv('../../data/storeddata/GDP_ValueResults.csv')
+
+    if indicator == "Retailsales":
+        IndicatorValues = pd.read_csv('../../data/storeddata/RTS_ValueResults.csv')
+
+    if indicator == "Ecommerce":
+        IndicatorValues = pd.read_csv('../../data/storeddata/Ecomm_ValueResults.csv')
+
+
+    IndicatorValues = IndicatorValues.tail(5)
+
+    for i in range(len(IndicatorValues)):
+        
+        if math.isnan(IndicatorValues.iloc[i,1]):
+            IndicatorValues.iloc[i,1] = IndicatorValues.iloc[i,3]
+        
+    IndicatorValues_Table = IndicatorValues.iloc[:,[0,1,4,5,6]]
+    IndicatorValues_Table.rename(columns={list(IndicatorValues_Table)[0]:'Date'}, inplace=True)
+    IndicatorValues_Table.rename(columns={list(IndicatorValues_Table)[2]:'Prediction Interval (2.5%)'}, inplace=True)
+    IndicatorValues_Table.rename(columns={list(IndicatorValues_Table)[3]:'Prediction Interval (97.5%)'}, inplace=True)
+
+    return IndicatorValues_Table
+
+ValueTable('GDP')
