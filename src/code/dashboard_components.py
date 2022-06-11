d95446590009e0cf3b6e5dfe58f3668cb23b2615
Jagdeep14 <sidhu.deepi87@gmail.com>
2022-06-10 17:05:03 -0700

    added dash components

--- src/code/dashboard_components.py
+++ src/code/dashboard_components.py
@@ -89,22 +89,6 @@ ec_download_button = html.Div([
 ])
 
 
-################################ Description button ################################
-about = html.Div(
-    [
-        dbc.Button("About", id="open-about"),
-        dbc.Modal(
-            [
-                dbc.ModalHeader(dbc.ModalTitle("Nowcasting Macroeconomic Indicators using Google Trends")),
-                dbc.ModalBody("Wow this thing takes up a lot of space..."),
-            ],
-            id="modal-about",
-            fullscreen=True,
-        ),
-    ]
-)
-
-
 ################################ Score card for Growth rate ################################
 card_growth_rate = html.Div(dbc.Card(
     [
@@ -160,3 +144,102 @@ value_plot_object  = html.Div([dcc.Graph(
     end_year = pd.to_datetime(datetime.today().strftime('%Y-%m-%d')).year),
     style={'border-width': '0', 'width': '100%', 'height': '500px'})
 ])
+
+
+################################ Description button ################################
+# Description button
+about = html.Div(
+    [
+        dbc.Button("About", id="open-about"),
+        dbc.Modal(
+            [
+                # Title
+                dbc.ModalHeader(dbc.ModalTitle(html.Div([
+                    html.H1(children='Nowcasting Macroeconomic Indicators using Google Trends',
+                            style={'color': '#935116 '}
+                            ),
+                    # html.Img(src='/../../assets/ubc.png')
+                ],
+
+                    style={'padding-top': '1%'}
+                ),)),
+
+                # Content
+                html.Div([
+
+                    # Overview
+                    html.H4(children='Overview'),
+                    html.H6(children='Macroeconomic factors are the key drivers of economy, and their timely information helps in good policy making. However, this information is available with a lag, for instance, the data for the present month’s GDP is generally published in the coming month/quarter which causes delay in decision-making. To overcome this issue of delayed information gave rise to nowcasting approach. This is what this project will serve. This project nowcasts the macroeconomic factors and uses the keywords from Google Trends to predict the indicators.'),
+                    html.Br(),
+
+                    # Description of indicators and Google Trends
+                    html.H4(children='Our Indicators and Predictors'),
+                    html.H6(
+                        children='Data set for this project are open ended and the short description about data is provided below:'),
+                    html.H6(children='1. Gross Domestic Product (GDP):',  style={
+                            "font-size": "20px", "text-decoration": "underline", "color": "red"}),
+                    html.H6(
+                        children='This dataset is a comma separated file containing the information about the monthly GDP. Link of file is as below:'),
+                    dcc.Markdown(
+                        '''[Gross Domestic Product](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3610010401)'''),
+                    html.H6(children='2. Retail Trade Sales:',  style={
+                            "font-size": "20px", "text-decoration": "underline", "color": "#F5B041 "}),
+                    html.H6(children='This is a comma separated data set containing the information about the retail sales trades as per the industry. Link of file is as below:'),
+                    dcc.Markdown(
+                        '''[Retail Trade Sales](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010000802)'''),
+                    html.H6(children='3. E - Commerce:',  style={
+                            "font-size": "20px", "text-decoration": "underline", "color": "green"}),
+                    html.H6(
+                        children='This is a comma separated dataset containing the information about the retail e-commerce sales. Link of file is as below:'),
+                    dcc.Markdown(
+                        '''[E - Commerce](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=2010007201)'''),
+                    html.H6(children='4. Google Trends API:',  style={
+                            "font-size": "20px", "text-decoration": "underline", "color": "#2980B9"}),
+                    html.H6(children='We have accessed Google Trends website to get real time data for the macroeconomic indicators. Different keywords, categories and subcategories are used to extract Google Trends predictors such as Economic crisis, loans, GPS, unemployment, affordable housing, economy news, agriculture, and forestry. Link of file is as below:'),
+                    dcc.Markdown(
+                        '''[Google Trends](https://trends.google.com/trends/?geo=CA)'''),
+
+                    # Info on nowcasting
+                    dbc.Card(
+                        [
+                            dbc.CardHeader(
+                                html.H2("Why is it Nowcasting and not Forecasting?")),
+                            dbc.CardBody(
+                                [
+                                    html.H5("Nowcast: Predicting the present. This project takes the present data and use that data to predict the values for indicators in present. Rather than predicting the future, we are predicting the present for the indicators.",
+                                            className="text-center")
+                                ]
+                            ),
+                        ],
+
+                        style={"width": "42rem", 'display': 'inline-block',
+                               "justify-content": "center", "border": "5px lightgray solid", 'background': '#FDF2E9'}
+                    ),
+                    dbc.Card(
+                        [
+                            dbc.CardHeader(
+                                html.H2("Why do we use Google Trends data?")),
+                            dbc.CardBody(
+                                [
+                                    html.H5("Google trends provide the keywords which are used as predictors in nowcasting the indicators. Trend is used for a particular keyword which is number of searches for that keyword per total number of searches.",
+                                            className="text-center")
+                                ]
+                            ),
+                        ],
+                        style={"width": "42rem", 'display': 'inline-block',
+                               "justify-content": "center", "border": "5px lightgray solid", 'background': '#FDF2E9'}
+                    ),
+                    html.Br(),
+                    html.Br(),
+
+
+                ], style={'padding-top': '1%', 'padding-left': '2%', 'padding-right': '2%', "background": "#D6EAF8"}),
+
+
+
+            ],
+            id="modal-about",
+            fullscreen=True,
+        ),
+    ]
+)
